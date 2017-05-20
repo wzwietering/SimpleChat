17fa85c4609268afa545f8d0b9be2060c3fedeaf
wzwietering <wzwietering@gmail.com>
2017-05-20 21:40:11 +0200

    Show users

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20170401175733) do
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
