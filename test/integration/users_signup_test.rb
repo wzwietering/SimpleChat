12788a8e7dfacb705d95bfaf688254df5c39b890
wzwietering <wzwietering@gmail.com>
2017-05-21 17:57:19 +0200

    Production stuff

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,13 +5,13 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "Bert",
-                                         email: "bert@hallo.nl",
-                                         password:              "hallo!",
-                                         password_confirmation: "hallo!" } }
+      post users_path, params: { user: { name:  "Dodo",
+                                         email: "dodo@hallo.nl",
+                                         password:              "testtest",
+                                         password_confirmation: "testtest" } }
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.FILL_IN
+    assert_not flash.nil?
   end
 end
